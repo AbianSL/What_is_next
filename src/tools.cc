d0c727edf48eab91e61e2a34bdde4fe420774755
AbianSL <abisanle@gmail.com>
2023-04-17 18:24:16 +0100

    Usage done

---
+++ src/tools.cc
@@ -0,0 +1,29 @@
+/**
+ * @file tools.cc 
+ * @author AbianSL
+ * @brief that file contains general functions for the program
+ * @version 0.1
+ * @date 2023-04-17
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
+#include <iostream>
+
+#include "tools.h"
+
+/**
+ * @brief This function prints the usage of the program
+ * 
+ * @param argc is the number of arguments 
+ * @param argv is the array of arguments
+ * @return int 0 if the program is executed correctly
+ */
+int Usage(int argc, char const *argv[]) {
+  if (argc != 1) {
+    std::cerr << "Usage: " << argv[0] << std::endl;
+    return 1;
+  }
+  return 0;
+}
---
+++ src/tools.h
@@ -0,0 +1,17 @@
+/**
+ * @file tools.h
+ * @author AbianSL
+ * @brief that file contains general functions for the program
+ * @version 0.1
+ * @date 2023-04-17
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
+#ifndef TOOLS_H
+#define TOOLS_H
+
+int Usage(int argc, char const *argv[]);
+
+#endif        // TOOLS_H
