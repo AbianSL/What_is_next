0dc8c04d4e7aaea6d52e478273fb7d79497f4184
AbianSL <abisanle@gmail.com>
2023-04-17 18:25:46 +0100

    starting the main funciton

--- src/main.cc
+++ src/main.cc
@@ -1,1 +1,18 @@
+/**
+ * @file main.cc
+ * @author AbianSL
+ * @brief is the main file of the project 
+ * @version 0.1
+ * @date 2023-04-17
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
+#include "tools.h"
+
+int main(int argc, char const *argv[]) {
+  Usage(argc, argv); 
+  return 0;
+}
 
