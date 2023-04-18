225e4bff32aa017da3e2f917508c61e60dfdb6a0
AbianSL <abisanle@gmail.com>
2023-04-18 02:18:14 +0100

    core dump fixed

--- src/Calculator/Calculator.cc
+++ src/Calculator/Calculator.cc
@@ -117,6 +117,8 @@ void Calculator::CalculateValueOfSubjects() {
   }
 }
 
+#include <iostream>
+
 /**
   * @brief Calculate the value of a subject
   * 
@@ -124,6 +126,10 @@ void Calculator::CalculateValueOfSubjects() {
   */
 void Calculator::CalculateValueOfSubjects(const unsigned& index) {
   auto actual_date = std::chrono::system_clock::now();
+  std::cout << subjects_practise_[index]->date_.time_since_epoch().count() << std::endl;
+  if (value_of_subjects_.size() < index + 1) {
+    value_of_subjects_.resize(index + 1);
+  }
   if (actual_date > subjects_practise_[index]->date_) {
     subjects_practise_[index] = 0;
   }
